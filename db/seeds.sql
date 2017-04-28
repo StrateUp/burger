f0cbf3a5371a75877f786483a33a280a5039efa2
KStrate <kaylaastl@Kaylas-MBP.attlocal.net>
2017-04-28 14:18:06 -0700

    almost working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie kale burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('flamed grill double cheeseburger', true);
